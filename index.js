e760a18a70c7160cb7b6690ad6d3acd64e13236b
Serhat Yılmaz <yilmaz.srhat@gmail.com>
2018-02-14 09:51:14 +0200

    Statik dosya belirleme

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "pug");
+//statik dosyaları belirtme
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
   res.render("index", { name: "Serhat", surname: "Yılmaz" });
