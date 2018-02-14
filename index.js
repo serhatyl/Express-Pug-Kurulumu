c0f95284e0379a3a59c1400bd6972c959d8652ba
Serhat Yılmaz <yilmaz.srhat@gmail.com>
2018-02-14 10:21:54 +0200

    Routing  1 - App.All ? * +

--- index.js
+++ index.js
@@ -5,16 +5,29 @@ app.set("view engine", "pug");
 //statik dosyaları belirtme
 app.use(express.static('public'));
 
+//Routing
+// ? - zorunlu olmayan
+// * yıldızın bulunduğu yere herhangi bir ifade gelebilir
+//+ soldaki ifadenin aynısı olduğu sürece çalışacaktır
+
 app.get("/", (req, res) => {
   res.render("index", { name: "Serhat", surname: "Yılmaz" });
 });
 
-app.get("/home", (req, res) => {
+app.get("/home?", (req, res) => {
   res.render("home");
 });
 
 app.get("/contact", (req, res) => {
   res.render("contact");
+});
+
+app.post('/contact', (req, res) => {
+  res.send('contact page post method');
+});
+
+app.all('/contact', (req, res) => {
+  res.send('selam burası app.all()');
 });
 
 app.listen(3000, () => {
