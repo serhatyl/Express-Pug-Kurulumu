677fca3a37ff35f827c364950b9f3a4c3f00314d
Serhat <yilmaz.srhat@gmail.com>
2018-02-14 10:31:43 +0300

    Block & Layout created

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-app.get('/', (req, res) => {
-    res.render('index', { name: 'Serhat', surname: 'Yılmaz' });
+app.get("/", (req, res) => {
+  res.render("index", { name: "Serhat", surname: "Yılmaz" });
+});
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 app.listen(3000, () => {
-    console.log('express server running');
+  console.log("express server running");
 });
